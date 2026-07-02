43ab722e370a8dadc9e9b80c3698b4de7e28da9e
keiji11070723-sys <keiji.1107.0723@gmail.com>
2026-07-03 07:57:04 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+import discord
+from discord.ext import commands
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} が起動しました！")
+
+bot.run(TOKEN)
